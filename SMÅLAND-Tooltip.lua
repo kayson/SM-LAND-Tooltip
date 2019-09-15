bcda2566cf56204974435821b8a67e39816b1e8a
nilssonvik <nilssonvik@gmail.com>
2019-09-15 15:25:28 +0200

    fix2

--- SMÅLAND-Tooltip.lua
+++ SMÅLAND-Tooltip.lua
@@ -2,6 +2,7 @@ GameTooltip:HookScript("OnTooltipSetUnit", function(self)
 	local name, unit = self:GetUnit()
 	if UnitIsPlayer("mouseover") and not UnitIsUnit("mouseover", "player") then
 		guildName, guildRankName, guildRankIndex = GetGuildInfo("mouseover");
+		
 		if guildName == "Three Crowns" then
 			self:AddLine("<" .. guildName .. ">", 1, 0, 0, true)
 		elseif guildName ~= nil then
